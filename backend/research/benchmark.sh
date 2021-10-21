fffdda2ee33e8eaed8c7ae80fbf6e00d2343800e
Tarek Ali <tarekali15@outlook.com>
2021-10-20 22:20:49 -0500

    rewrote comments n such

--- backend/research/benchmark.sh
+++ backend/research/benchmark.sh
@@ -1,5 +1,7 @@
 echo "Starting Syn-Scan tests"
 echo "-----------------------"
 sudo nmap -sS 142.250.113.138
-# look for open ports ^, nmap vuln scan accordingly 
+# 1. look for open ports under 10k range through nmap -sS -p1-10000 type functionality, may look into script scanning 
+# 2. encode scan result into JSON, parse thru middleware
+# 3. from middleware, attempt attacks/vuln detection based off of available ports/protocols etc
 echo "-----------------------"
