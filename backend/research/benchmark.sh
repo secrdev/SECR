391057ae3861a3d13a7f228c29f283565c9cab5d
Tarek Ali <tarekali15@outlook.com>
2021-10-20 21:02:21 -0500

    updated comment

--- backend/research/benchmark.sh
+++ backend/research/benchmark.sh
@@ -1,5 +1,5 @@
 echo "Starting Syn-Scan tests"
 echo "-----------------------"
 sudo nmap -sS 142.250.113.138
-# look for open ports ^, scan accordingly 
+# look for open ports ^, nmap vuln scan accordingly 
 echo "-----------------------"
