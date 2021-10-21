63026e672c20e61d887e6ff08fdb4c3f35fd703d
Tarek Ali <tarekali15@outlook.com>
2021-10-20 20:13:19 -0500

    more ports to zmap

--- backend/research/benchmark.sh
+++ backend/research/benchmark.sh
@@ -2,4 +2,4 @@ echo "Starting Syn-Scan tests"
 echo "-----------------------"
 sudo nmap -sS 142.250.113.138
 echo "-----------------------"
-sudo zmap -p 80 142.250.113.138
+sudo zmap -B 10M -p 80 -n 10000 142.250.113.138
