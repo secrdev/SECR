b2b5e2c425c6fad4b0bb49806e988655f9286d85
Tarek Ali <tarekali15@outlook.com>
2021-10-23 11:35:46 -0500

    tags pretest

--- backend/api/api.go
+++ backend/api/api.go
@@ -7,12 +7,12 @@ type VulnReport struct {
 
 // Vulnerability is a struct representation of a detected vulnerability thru nmap
 type Vulnerability struct {
-	Port      uint32
-	Service   string
-	ID        string
-	IsExploit bool
-	Type      string
-	Cvss      uint8
+	Port      uint32 `xml:"portid"`
+	Service   string `xml:"output"`
+	ID        string `xml:"id"`
+	IsExploit bool   `xml:"is_exploit"`
+	Type      string `xml:"type"`
+	Cvss      uint8  `xml:"cvss"`
 }
 
 func ReportHandler() {
