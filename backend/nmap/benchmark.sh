4789ff7284e04d299c01559cb294f40553c2c068
Tarek Ali <tarekali15@outlook.com>
2021-10-22 22:27:37 -0500

    removed vulnscan

--- backend/nmap/benchmark.sh
+++
@@ -1,7 +0,0 @@
-echo "Starting Syn-Scan tests"
-echo "-----------------------"
-sudo nmap -sS $1 
-# 1. look for open ports under 10k range through nmap -sS -p1-10000 type functionality, may look into script scanning 
-# 2. encode scan result into JSON, parse thru middleware
-# 3. from middleware, attempt attacks/vuln detection based off of available ports/protocols etc
-echo "-----------------------"
