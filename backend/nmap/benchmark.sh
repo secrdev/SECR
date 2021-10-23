cb2ca2044e7c9f9a8350c14666d18a853871aa6d
Tarek Ali <tarekali15@outlook.com>
2021-10-22 22:11:17 -0500

    changed benchmark and such

--- backend/nmap/benchmark.sh
+++ backend/nmap/benchmark.sh
@@ -1,6 +1,6 @@
 echo "Starting Syn-Scan tests"
 echo "-----------------------"
-sudo nmap -sS 142.250.113.138
+sudo nmap -sS $1 
 # 1. look for open ports under 10k range through nmap -sS -p1-10000 type functionality, may look into script scanning 
 # 2. encode scan result into JSON, parse thru middleware
 # 3. from middleware, attempt attacks/vuln detection based off of available ports/protocols etc
--- backend/nmap/vulnscan.sh
+++ backend/nmap/vulnscan.sh
@@ -1,4 +1,4 @@
 echo "Starting Vulnscan"
 echo "-----------------------"
-sudo nmap -T5 -sV -Pn $1 --script=nmap-vulners/vulners.nse -p22, 80, 443
+sudo nmap -T5 -sV -Pn $1 --script=nmap-vulners/vulners.nse -p22,80,443
 echo "-----------------------"
