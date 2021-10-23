17150af7e0de309c2deea26132c4507c5181af9d
Tarek Ali <tarekali15@outlook.com>
2021-10-23 10:53:08 -0500

    comments

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,9 +1,11 @@
 package api
 
+// VulnerabilityReport is an entire report (collection of vulnerabilities)
 type VulnReport struct {
 	Vulnerabilities []Vulnerability
 }
 
+// Vulnerability is a struct representation of a detected vulnerability thru nmap
 type Vulnerability struct {
 	Port    uint32
 	Service string
