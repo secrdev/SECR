a957d8a3f4f6784ac2c51632adead5791ea86b57
Tarek Ali <tarekali15@outlook.com>
2021-10-22 22:12:52 -0500

    comment

--- backend/nmap/vulnscan.sh
+++ backend/nmap/vulnscan.sh
@@ -1,4 +1,5 @@
 echo "Starting Vulnscan"
 echo "-----------------------"
+# Syn Scan for Port numbers then pipe into vuln scan w/ depending tier and open port #'s
 sudo nmap -T5 -sV -Pn $1 --script=nmap-vulners/vulners.nse -p22,80,443
 echo "-----------------------"
