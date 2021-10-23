e53799c90df15a75d7c7e14ea06e0e5cb31e1c73
Tarek Ali <tarekali15@outlook.com>
2021-10-22 22:27:27 -0500

    simplifying vulnscan

--- backend/nmap/vulnscan.sh
+++ backend/nmap/vulnscan.sh
@@ -1,4 +1,1 @@
-echo "Starting Vulnscan"
-echo "-----------------------"
-sudo nmap -T5 -sV -Pn $1 --script=nmap-vulners/vulners.nse -p22,80,443
-echo "-----------------------"
+sudo nmap -sV $1 --script=nmap-vulners/vulners.nse
