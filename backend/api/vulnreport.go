f1a3f919776e0c2a7f4fec5dd8efe5064314aa55
Tarek Ali <tarekali15@outlook.com>
2021-10-25 21:25:06 -0500

    commented some stuff out; trying to get back 2 basics for now

--- backend/api/vulnreport.go
+++ backend/api/vulnreport.go
@@ -19,12 +19,13 @@ type Vulnerability struct {
 }
 
 func ExecuteVulnscan(URL string) error {
-	var report Vulnerability
+	// var report Vulnerability
 	output, err := exec.Command("/bin/sh", "./nmap/vulnscan.sh", URL).Output()
 	if err != nil {
 		return err
 	}
-	xml.Unmarshal(output, &report)
-	fmt.Println(report.Cvss, report.ID, report.Type, report.IsExploit)
+	// xml.Unmarshal(output, &report)
+	// fmt.Println(report.Cvss, report.ID, report.Type, report.IsExploit)
+	fmt.Println(string(output))
 	return nil
 }
