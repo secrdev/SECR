97ea1c23e492618d4492b049c8ac34469ec86f57
Tarek Ali <tarekali15@outlook.com>
2021-10-31 14:32:14 -0500

    RESTful api pretest

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,12 +1,16 @@
 package api
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func HandleReportRequest(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query().Get("url")
-	ExecuteVulnscan(url)
+	scan, err := ExecuteVulnscan(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(scan)
 }
-
-
