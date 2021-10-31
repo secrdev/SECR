c0c8b3f64fc8a6de5bbbf000ed0ea8302fdfe7e9
Tarek Ali <tarekali15@outlook.com>
2021-10-30 22:53:11 -0500

    forget xml lol

--- backend/api/vulnreport.go
+++ backend/api/vulnreport.go
@@ -1,28 +1,15 @@
 package api
 
 import (
-	"encoding/xml"
 	"fmt"
 	"os/exec"
 )
 
-type Table struct {
-	XMLName xml.Name `xml:"table"`
-	Elem    []Elem   `xml:"tag"`
-}
-
-type Elem struct {
-	Key   string `xml:"key,attr"`
-	Value string `xml:",chardata"`
-}
-
 func ExecuteVulnscan(URL string) error {
-	var elem Table
 	output, err := exec.Command("/bin/sh", "./nmap/vulnscan.sh", URL).Output()
 	if err != nil {
 		return err
 	}
-	xml.Unmarshal([]byte(output), &elem)
-	fmt.Println(elem)
+	fmt.Println(output)
 	return nil
 }
