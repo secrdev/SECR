14d578170f4970883b9570fbe8d98fa1805dcace
Tarek Ali <tarekali15@outlook.com>
2021-10-31 00:05:25 -0500

    printed regexp for CVEs; need to make it so that it doesnt repeat them

--- backend/api/vulnreport.go
+++ backend/api/vulnreport.go
@@ -3,13 +3,19 @@ package api
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
 )
+
+var regexPatterns = map[string]string{
+	"cve": `CVE-\d{4}-\d{1,10}`,
+}
 
 func ExecuteVulnscan(URL string) error {
 	output, err := exec.Command("/bin/sh", "./nmap/vulnscan.sh", URL).Output()
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(output))
+	re := regexp.MustCompile(regexPatterns["cve"])
+	fmt.Printf("%q\n", (re.FindAll(output, -1)))
 	return nil
 }
