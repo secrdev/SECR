0c2009ca9cd257f4c25e1fda6bdafb8dbb2b2249
Tarek Ali <tarekali15@outlook.com>
2021-11-03 08:41:44 -0500

    global alignment and scaling...WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="Form-container">
-        <h1 className="Logo">SECR</h1>
-        {/* <input className="URL-input" type="text" placeholder="Enter URL..." /> */}
-      </div>
+      <h1 className="Logo">SECR</h1>
+      <input className="URL-input" type="text" placeholder="Enter URL..." />
     </div>
   );
 }
