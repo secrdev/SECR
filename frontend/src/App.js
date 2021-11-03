95b2bd6a8b17ddd4cbeac2da747b56a558ba4286
Tarek Ali <tarekali15@outlook.com>
2021-11-02 20:09:03 -0500

    going to redo CSS tmrw...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="Logo">SECR</h1>
-      <input className="URL-input" type="text" placeholder="Enter URL..." />
+      <div className="Form-container">
+        <h1 className="Logo">SECR</h1>
+        {/* <input className="URL-input" type="text" placeholder="Enter URL..." /> */}
+      </div>
     </div>
   );
 }
