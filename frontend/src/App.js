cdb617018ef54c1d506f94be0ae84d0daf20b290
Tarek Ali <tarekali15@outlook.com>
2021-11-03 19:04:40 -0500

    stylized submit button, working on alignment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="Content">
       <h1 className="Logo">SECR</h1>
-      <input className="URL-input" type="text" placeholder="Enter URL" />
+      <input className="URL-input" type="text" placeholder="Enter URL" /><input className="URL-submit" type="submit" value="Scan" />
     </div>
   );
 }
