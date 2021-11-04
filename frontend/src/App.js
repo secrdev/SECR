663589adc0956eec7ef4d92b7015fd0360bedd2a
Tarek Ali <tarekali15@outlook.com>
2021-11-03 19:31:57 -0500

    some style changes/internal flexbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="Content">
       <h1 className="Logo">SECR</h1>
-      <input className="URL-input" type="text" placeholder="Enter URL" /><input className="URL-submit" type="submit" value="Scan" />
+      <div className="Input-container">
+        <input className="URL-input" type="text" placeholder="Enter URL" /><input className="URL-submit" type="submit" value="Scan" />
+      </div>
     </div>
   );
 }
