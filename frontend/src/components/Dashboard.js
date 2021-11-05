e07b1f2b22e64b97d61b4f34c3faa45b9c05ca61
Tarek Ali <tarekali15@outlook.com>
2021-11-04 20:18:04 -0500

    progress bar design

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,25 @@
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
 export default function Dashboard() {
     return (
         <div className="Content">
-            <h1>hello world</h1>
+            <CircularProgressbar value={25} text={'25%'} background={true} styles={{
+                root: {},
+                path: {
+                    stroke: `rgba(255, 00, 00)`,
+                },
+                trail: {
+                    stroke: '#2b2b2b',
+                },
+                text: {
+                    fill: '#ff0000',
+                },
+                background: {
+                    fill: '#2b2b2b',
+                }
+            }} />
         </div>
     )
 }
