66d33c22804765d94141273bb787d82659838f22
Tarek Ali <tarekali15@outlook.com>
2021-11-05 08:31:49 -0500

    div and styles for circularprogressbar

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,21 +5,23 @@ import 'react-circular-progressbar/dist/styles.css';
 export default function Dashboard() {
     return (
         <div className="Content">
-            <CircularProgressbar value={25} text={'25%'} background={true} styles={{
-                root: {},
-                path: {
-                    stroke: `rgba(255, 00, 00)`,
-                },
-                trail: {
-                    stroke: '#2b2b2b',
-                },
-                text: {
-                    fill: '#ff0000',
-                },
-                background: {
-                    fill: '#2b2b2b',
-                }
-            }} />
+            <div style={{ width: 250, height: 250 }}>
+                <CircularProgressbar value={25} text={'25%'} background={true} styles={{
+                    root: {},
+                    path: {
+                        stroke: `rgba(255, 00, 00)`,
+                    },
+                    trail: {
+                        stroke: '#2b2b2b',
+                    },
+                    text: {
+                        fill: '#ff0000',
+                    },
+                    background: {
+                        fill: '#2b2b2b',
+                    }
+                }} />
+            </div>
         </div>
     )
 }
