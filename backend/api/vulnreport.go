18ff0a10679c7666c598de4b20c5c020cb874b9e
Tarek Ali <tarekali15@outlook.com>
2021-11-08 17:23:19 -0600

    renamed things accordingly

--- backend/api/vulnreport.go
+++ backend/api/vulnreport.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Report struct {
+	Port    string   `json:"port"`
 	Service string   `json:"service"`
 	Vulns   []string `json:"vulns"`
 }
 
 var regexPatterns = map[string]string{
-	"cve":     `https://vulners.com/cve/CVE-\d{4}-\d{1,10}`,
-	"service": `\d{0,5}\/tcp`,
+	"cve":  `https://vulners.com/cve/CVE-\d{4}-\d{1,10}`,
+	"port": `\d{0,5}\/tcp`,
 }
 
 func ExecuteVulnscan(URL string) (Report, error) {
@@ -23,8 +24,8 @@ func ExecuteVulnscan(URL string) (Report, error) {
 		return report, err
 	}
 	log.Println(string(output))
-	patterns := []*regexp.Regexp{regexp.MustCompile(regexPatterns["service"]), regexp.MustCompile(regexPatterns["cve"])}
-	report.Service = string(patterns[0].Find(output))
+	patterns := []*regexp.Regexp{regexp.MustCompile(regexPatterns["port"]), regexp.MustCompile(regexPatterns["cve"])}
+	report.Port = string(patterns[0].Find(output))
 	for _, v := range patterns[1].FindAll(output, -1) {
 		report.Vulns = append(report.Vulns, string(v))
 	}
