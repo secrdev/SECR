802c3bdb64c82bef89f029c130d0cdc6c1c00d89
Tarek Ali <tarekali15@outlook.com>
2021-11-08 13:11:44 -0600

    log.Fatal wrap

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"secr/api"
 )
 
 func main() {
 	http.HandleFunc("/", api.HandleReportRequest)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
