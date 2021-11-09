8316568d2d63c6009fc94bd45504ce1712f7c6ad
Tarek Ali <tarekali15@outlook.com>
2021-11-08 20:05:46 -0600

    added conditions relative to UI components

--- frontend/src/utils/Severity.js
+++ frontend/src/utils/Severity.js
@@ -2,4 +2,11 @@
     How Severity scoring (total % out of 100) will initially work:
         - Avg. Severity Score (CWE) * 10
         - Factor in Quantity?
+    Conditions relative to UI components:
+        if score < 50%:
+            - Red Gradient
+        elseif score >= 50%:
+            - Green Gradient
+        elseif score nil:
+            - Green Gradient + Special message showing no found vulnerabilities in infra
 */
