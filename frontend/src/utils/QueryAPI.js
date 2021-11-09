67a4c395aecc5a6dcb449fad59fd163f57b2021a
Tarek Ali <tarekali15@outlook.com>
2021-11-08 21:11:43 -0600

    removed timeout

--- frontend/src/utils/QueryAPI.js
+++ frontend/src/utils/QueryAPI.js
@@ -5,7 +5,6 @@ export default function QueryAPI(url) {
         axios.get({
             url: `localhost:8080/?url=${url}`,
             method: 'get',
-            timeout: 90000,
         }).then(function (response) {
             console.log(response.data);
         }).catch(function (error) {
