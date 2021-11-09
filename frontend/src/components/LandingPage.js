c91c06b6221e83ba32b9cd277b706d023bfa71bb
Tarek Ali <tarekali15@outlook.com>
2021-11-08 21:44:14 -0600

    stopped defautl reloading for form and custom behavior and such

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -3,12 +3,22 @@ import QueryAPI from '../utils/QueryAPI';
 
 export default function LandingPage() {
     const [url, seturl] = useState('');
+
+    function handleUrlChange(e) {
+        seturl(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        QueryAPI(url);
+    }
+
     return (
         <div className="Landing-page">
             <h1 className="Logo">SECR</h1>
             <div className="Input-container">
-                <form>
-                    <input className="URL-input" value={url} type="text" placeholder="Enter URL" onInput={e => seturl(e.target.value)} /><input className="URL-submit" type="submit" value="Scan" onClick={QueryAPI(url)} />
+                <form onSubmit={handleSubmit}>
+                    <input className="URL-input" value={url} type="text" placeholder="Enter URL" onChange={handleUrlChange} /><input className="URL-submit" type="submit" value="Scan" />
                 </form>
             </div>
         </div>
