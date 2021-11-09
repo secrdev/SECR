839e2ad52494781d1a80a29c2eaaeadd63132c57
Tarek Ali <tarekali15@outlook.com>
2021-11-08 21:08:23 -0600

    changing up sum internals workin on it overall....

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -7,7 +7,7 @@ export default function LandingPage() {
         <div className="Landing-page">
             <h1 className="Logo">SECR</h1>
             <div className="Input-container">
-                <form onSubmit={console.log(url)}>
+                <form>
                     <input className="URL-input" value={url} type="text" placeholder="Enter URL" onInput={e => seturl(e.target.value)} /><input className="URL-submit" type="submit" value="Scan" onClick={QueryAPI(url)} />
                 </form>
             </div>
--- frontend/src/utils/QueryAPI.js
+++ frontend/src/utils/QueryAPI.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 export default function QueryAPI(url) {
-    axios.get({
-        url: `localhost:8080/?url=${url}`,
-        method: 'get',
-        timeout: 90000,
-    }).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.log(error);
-    });
+    if (url != null) {
+        axios.get({
+            url: `localhost:8080/?url=${url}`,
+            method: 'get',
+            timeout: 90000,
+        }).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
 }
