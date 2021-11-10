e04b18933311e909d8fa14704546174e2cb1f436
Tarek Ali <tarekali15@outlook.com>
2021-11-10 13:51:19 -0600

    revert

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,9 +1,7 @@
 import { useState } from 'react';
-import Dashboard from './Dashboard';
 
 export default function LandingPage() {
     const [url, seturl] = useState('');
-    const [isSubmitted, setisSubmitted] = useState(false);
 
     function handleUrlChange(e) {
         seturl(e.target.value);
@@ -11,25 +9,16 @@ export default function LandingPage() {
 
     function handleSubmit(e) {
         e.preventDefault();
-        setisSubmitted(true);
-    }
-
-    const Land = () => {
-        return (
-            <div className="Landing-page">
-                <h1 className="Logo">SECR</h1>
-                <div className="Input-container">
-                    <form onSubmit={handleSubmit}>
-                        <input className="URL-input" value={url} type="text" placeholder="Enter URL" onChange={handleUrlChange} /><input className="URL-submit" type="submit" value="Scan" />
-                    </form>
-                </div>
-            </div>
-        )
     }
 
     return (
-        <>
-            {(!isSubmitted) ? <Land /> : <Dashboard url={url} />}
-        </>
+        <div className="Landing-page">
+            <h1 className="Logo">SECR</h1>
+            <div className="Input-container">
+                <form onSubmit={handleSubmit}>
+                    <input className="URL-input" value={url} type="text" placeholder="Enter URL" onChange={handleUrlChange} /><input className="URL-submit" type="submit" value="Scan" />
+                </form>
+            </div>
+        </div>
     );
 }
