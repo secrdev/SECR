6223ebbf8845c1bb1a151396f95fc2ace56e7099
Tarek Ali <tarekali15@outlook.com>
2021-11-10 13:27:27 -0600

    render conditionals

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -3,6 +3,7 @@ import Dashboard from './Dashboard';
 
 export default function LandingPage() {
     const [url, seturl] = useState('');
+    const [isSubmitted, setisSubmitted] = useState(false);
 
     function handleUrlChange(e) {
         seturl(e.target.value);
@@ -10,19 +11,25 @@ export default function LandingPage() {
 
     function handleSubmit(e) {
         e.preventDefault();
+        setisSubmitted(true);
+    }
+
+    const Land = () => {
         return (
-            <Dashboard url={url} />
+            <div className="Landing-page">
+                <h1 className="Logo">SECR</h1>
+                <div className="Input-container">
+                    <form onSubmit={handleSubmit}>
+                        <input className="URL-input" value={url} type="text" placeholder="Enter URL" onChange={handleUrlChange} /><input className="URL-submit" type="submit" value="Scan" />
+                    </form>
+                </div>
+            </div>
         )
     }
 
     return (
-        <div className="Landing-page">
-            <h1 className="Logo">SECR</h1>
-            <div className="Input-container">
-                <form onSubmit={handleSubmit}>
-                    <input className="URL-input" value={url} type="text" placeholder="Enter URL" onChange={handleUrlChange} /><input className="URL-submit" type="submit" value="Scan" />
-                </form>
-            </div>
-        </div>
+        <>
+            {(!isSubmitted) ? <Land /> : <Dashboard url={url} />}
+        </>
     );
 }
