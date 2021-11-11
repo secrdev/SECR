9fc01e83decd3b2262828a41a5a18b62b04422c4
Tarek Ali <tarekali15@outlook.com>
2021-11-11 11:19:49 -0600

    wtfff elon musk

--- frontend/src/utils/QueryAPI.js
+++ frontend/src/utils/QueryAPI.js
@@ -4,9 +4,7 @@ export default async function QueryAPI(url) {
     if (url != null) {
         try {
             const response = await axios.get('http://localhost:8080', { params: { url: url } });
-            if (response.status === 200) {
-                return response.data;
-            }
+            return response.data;
         } catch (error) {
             console.log(error);
         }
