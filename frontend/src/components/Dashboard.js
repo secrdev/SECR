943b6ace62571dc53b9f3092a4a5fa26b1826c15
Tarek Ali <tarekali15@outlook.com>
2021-11-17 13:17:21 -0600

    yuh

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -2,8 +2,6 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import QueryAPI from '../utils/QueryAPI';
 
 export default function Dashboard({ url }) {
-    const res = QueryAPI("127.0.0.1");
-    console.log(res)
     return (
         <div className="Dashboard">
             <div className="Progress-bar">
--- frontend/src/utils/QueryAPI.js
+++ frontend/src/utils/QueryAPI.js
@@ -3,7 +3,15 @@ import axios from 'axios';
 export default async function QueryAPI(url) {
     if (url != null) {
         await axios.get('http://localhost:8080', { params: { url: url } }).then((response) => {
-            return response.data;
+            response.data.map((listValue, index) => {
+                return (
+                    <tr key={index}>
+                        <td>{listValue.port}</td>
+                        <td>{listValue.service}</td>
+                        <td>{listValue.vulns}</td>
+                    </tr>
+                )
+            });
         }).catch(error => {
             console.log(error);
         });
