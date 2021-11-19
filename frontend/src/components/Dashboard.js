ff504e130319da22934e7166c02e4767cf228ff8
Tarek Ali <tarekali15@outlook.com>
2021-11-18 22:09:13 -0600

    big wtf component I need to sort out

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import CalculateSecurityScore from '../utils/CalculateSecurityScore';
+import Loadscreen from './Loadscreen';
 
 export default function Dashboard({ url }) {
     const { data, isLoading, error } = useFetchData({ url: url })
@@ -15,43 +16,46 @@ export default function Dashboard({ url }) {
 
     return (
         <div className="Dashboard">
-            <div className="Progress-bar">
-                <CircularProgressbar value={score} text={`${score}%`} background={true} styles={{
-                    root: {},
-                    path: {
-                        stroke: `rgba(255, 00, 00)`,
-                    },
-                    trail: {
-                        stroke: '#2b2b2b',
-                    },
-                    text: {
-                        fill: '#ff0000',
-                    },
-                    background: {
-                        fill: '#2b2b2b',
-                    }
-                }} />
-            </div>
-            <div className="Table-container">
-                <table className="Table">
-                    <tr>
-                        <th>Port</th>
-                        <th>Service</th>
-                        <th>Vulnerability</th>
-                        <th>Description</th>
-                    </tr>
-                    {isLoading ? <h4>Loading...</h4> : <tbody>
-                        {data.vulns.map((vuln, id) => (
-                            <tr key={id}>
-                                <td>{data.port}</td>
-                                <td>{data.service.replace('_http-server-header:', '')}</td>
-                                <td>{vuln.replace('https://vulners.com/cve/', '')}</td>
-                                <td>ap_escape_quotes() may write beyond the end of a buffer when given malicious input. No included modules pass untrusted data to these functions, but third-party / external modules may. This issue affects Apache HTTP Server 2.4.48 and earlier.</td>
+            {!isLoading ? (
+                <>
+                    <div className="Progress-bar">
+                        <CircularProgressbar value={score} text={`${score}%`} background={true} styles={{
+                            root: {},
+                            path: {
+                                stroke: `rgba(255, 00, 00)`,
+                            },
+                            trail: {
+                                stroke: '#2b2b2b',
+                            },
+                            text: {
+                                fill: '#ff0000',
+                            },
+                            background: {
+                                fill: '#2b2b2b',
+                            }
+                        }} />
+                    </div><div className="Table-container">
+                        <table className="Table">
+                            <tr>
+                                <th>Port</th>
+                                <th>Service</th>
+                                <th>Vulnerability</th>
+                                <th>Description</th>
                             </tr>
-                        ))}
-                    </tbody>}
-                </table>
-            </div>
+                            {isLoading ? <h4>Loading...</h4> : <tbody>
+                                {data.vulns.map((vuln, id) => (
+                                    <tr key={id}>
+                                        <td>{data.port}</td>
+                                        <td>{data.service.replace('_http-server-header:', '')}</td>
+                                        <td>{vuln.replace('https://vulners.com/cve/', '')}</td>
+                                        <td>ap_escape_quotes() may write beyond the end of a buffer when given malicious input. No included modules pass untrusted data to these functions, but third-party / external modules may. This issue affects Apache HTTP Server 2.4.48 and earlier.</td>
+                                    </tr>
+                                ))}
+                            </tbody>}
+                        </table>
+                    </div>
+                </>
+            ) : (<Loadscreen />)}
         </div>
     )
 };
