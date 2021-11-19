39c81d8ada191a3bb3485c1e417c7fdc70977bd2
Tarek Ali <tarekali15@outlook.com>
2021-11-18 19:55:13 -0600

    export default

--- frontend/src/utils/CalculateSecurityScore.js
+++ frontend/src/utils/CalculateSecurityScore.js
@@ -1,4 +1,4 @@
-function CalculateSecurityScore(vulnCount, portCount) {
+export default function CalculateSecurityScore(vulnCount, portCount) {
     const securityScore = math.round((portCount / vulnCount) * 100);
     if (securityScore > 100 || vulnCount === 0) {
         securityScore = 100;
