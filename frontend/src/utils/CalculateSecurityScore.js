34a2dca4b30c1eab26f1c8d1fd24d4dddca0e39e
Tarek Ali <tarekali15@outlook.com>
2021-11-18 20:22:19 -0600

    changed to let instead of const cause reassignment and such

--- frontend/src/utils/CalculateSecurityScore.js
+++ frontend/src/utils/CalculateSecurityScore.js
@@ -1,5 +1,5 @@
 export default function CalculateSecurityScore(portCount, vulnCount) {
-    const securityScore = Math.round((portCount / vulnCount) * 100);
+    let securityScore = Math.round((portCount / vulnCount) * 100);
     if (securityScore > 100 || vulnCount === 0) {
         securityScore = 100;
         return securityScore;
